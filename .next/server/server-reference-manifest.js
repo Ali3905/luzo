7462fe2d14a376521ae2c96b3e60a42cd4c170df
Ali3905 <muhammadali30905@gmail.com>
2024-10-05 12:11:19 +0500

    make the service details dynamic

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bfgyVyupbiT7kpVx40bMIlagIWRktALoUj6ct4HZhN8=\"\n}"
--- src/app/partner-details/[id]/Services.jsx
+++ src/app/partner-details/[id]/Services.jsx
@@ -1,8 +1,13 @@
+"use client"
+
 // components/ServicesSection.js
 import Image from 'next/image';
+import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
 
 const ServicesSection = ({ services }) => {
+  const router = useRouter()
+  const { id } = useParams()
     // const services = [
     //     { id: 1, title: 'Hair-Cut, Wash & Style', image: '/img/icon.webp' },
     //     { id: 2, title: 'Hair Colour', image: '/img/icon.webp' },
@@ -18,7 +23,7 @@ const ServicesSection = ({ services }) => {
       <h2 className="text-2xl sm:text-[32px] font-semibold mb-4">Services</h2>
       <div className="grid grid-cols-3 sm:grid-cols-5 gap-2">
         {services?.map((service) => (
-          <div key={service.id} className="text-center">
+          <div key={service.id} className="text-center" onClick={()=>router.push(`/partner-details/${id}/service-details/service-${service.id}`)} >
             <Image src={service.image_url} alt={service.name} width={100} height={100} className="h-20 sm:h-40 sm:w-40 w-20 rounded-lg mx-auto" />
             <p className="text-[14px] sm:text-[18px] mt-2">{service.name}</p>
           </div>
